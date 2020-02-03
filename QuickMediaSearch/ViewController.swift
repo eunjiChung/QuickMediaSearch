9f53b0c81697334df583b8c0f568440f54235970
tskim <tskim@carby.co.kr>
2020-02-03 22:28:36 +0900

    init commit

---
+++ QuickMediaSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickMediaSearch
+//
+//  Created by Tyler on 2020/02/03.
+//  Copyright © 2020 Assin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
